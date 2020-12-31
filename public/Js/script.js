017fc7e6760a3f6c29a931c0db5c9622094a04bf
Shola Taiwo <m_webmaster007@yahoo.co.uk>
2020-12-31 22:15:36 +0100

    added text to the about page and more weather info

--- public/Js/script.js
+++ public/Js/script.js
@@ -4,19 +4,23 @@ const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector('#mess-1')
 const messageTwo = document.querySelector('#mess-2')
+const messageThree = document.querySelector('#mess-3')
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
   messageOne.textContent = "Loading..."
+  messageThree.textContent = ""
   messageTwo.textContent = ""
-  fetch(`/weather?address=${location}`).then((res) =>
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then((res) =>
     res.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
       } else {
-        messageOne.textContent = data.forcast.temp
+        messageOne.textContent = `Current Temp: ${data.forcast.temp} deg celcius`
         messageTwo.textContent = data.location
+        messageThree.textContent = `${data.forcast.desc} and current humidity is ${data.forcast.humidity} `
       }
     })
   );
--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -1,18 +1,21 @@
-const request = require('request')
+const request = require("request");
 
 const forcast = (lat, long, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=21ecc042350950242f3ddd5003684f07&query=${lat},${long}`
-    request({url: url, json: true}, (error, response)=>{
-        if(error){
-            callback('unable to connect to the weatherstack service', undefined)
-        }
-        else if(response.body.error){
-            callback('unable to find location', undefined)
-        }
-        else {
-            callback(undefined, {temp: response.body.current.temperature, feelLike: response.body.current.feelslike})
-        }
-    })
-}
+  const url = `http://api.weatherstack.com/current?access_key=21ecc042350950242f3ddd5003684f07&query=${lat},${long}`;
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("unable to connect to the weatherstack service", undefined);
+    } else if (response.body.error) {
+      callback("unable to find location", undefined);
+    } else {
+      callback(undefined, {
+        temp: response.body.current.temperature,
+        feelLike: response.body.current.feelslike,
+        desc: response.body.current.weather_descriptions,
+        humidity: response.body.current.humidity
+      });
+    }
+  });
+};
 
-module.exports = forcast
+module.exports = forcast;
